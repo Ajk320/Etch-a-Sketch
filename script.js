b295c6d1e8a94c454f21e5b2aad0e5b5ba7e14e0
abhijay <abhijay320@gmail.com>
2024-01-06 19:13:51 +0530

    Setting grid size with 'GridSize' is now functioning

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
 const gridContainer = document.querySelector('#gridContainer');
+let gridItems;
+let currentGridSize;
+let newGridSize;
 
-for(let i = 0; i< 16*16; i++){
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
-    gridContainer.appendChild(gridItem);
+function createGrid(size) {
+    // Clear previous grid
+    gridContainer.innerHTML = '';
+    newGridSize = size * 32;
+    gridContainer.style.maxWidth = `${newGridSize}px`;
+
+
+    for (let i = 0; i < size * size; i++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        gridContainer.appendChild(gridItem);
+    }
+
+    gridItems = document.getElementsByClassName('grid-item');
+    Array.from(gridItems).forEach(gridItem => {
+        // gridItem.style.flexBasis = flexBasis;
+        gridItem.addEventListener("mouseover", changeColor);
+    });
+
+    currentGridSize = size;
+    console.log(currentGridSize);
 }
 
-const gridItems = document.getElementsByClassName('grid-item');
-Array.from(gridItems).forEach(gridItem => {
-    gridItem.addEventListener("mouseover", changeColor);
-});
-
-function changeColor(){
+function changeColor() {
     this.style.background = "black";
 }
+
+const gridSizeButton = document.getElementById('gridSizePrompt');
+
+gridSizeButton.addEventListener("click", changeGridSize);
+
+function changeGridSize() {
+    let newGridSize = prompt("Enter the new grid size");
+
+    if (newGridSize && newGridSize > 0) {
+       
+        createGrid(newGridSize);
+    }
+}
+
+
+
+// initial grid creation
+createGrid(16);
